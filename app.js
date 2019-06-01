3e91bf16d869c83bd7c776de01a4e5591fc4520c
rafaella nicoletti <rafaella_np@hotmail.com>
2019-06-01 11:19:37 -0400

    database working

--- app.js
+++ app.js
@@ -8,3 +8,40 @@ var firebaseConfig = {
     messagingSenderId: "351574340721",
     appId: "1:351574340721:web:0842a607cd1fd5f3"
 };
+
+firebase.initializeApp(firebaseConfig);
+
+var employer= "";
+var role= "";
+var date= 0; 
+var monthlyRate= 0;
+
+$("#addUser").on("click",function(){
+    event.preventDefault();
+    employer = $("#employerNameInput").val().trim();
+    role = $("#roleInput").val().trim();
+    date = $("#startDateInput").val().trim();
+    monthlyRate = $("#monthlyRateInput").val().trim();
+
+    firebase.database().ref().set({
+        employer:employer,
+        role:role,
+        date:date,
+        monthlyRate:monthlyRate,
+    })
+
+})
+
+firebase.database().ref().on("value", function (snapshot) {
+    $("#employerDisplay").html(snapshot.val().employer);
+    $("#roleDisplay").html(snapshot.val().role);
+    $("#startDisplay").html(snapshot.val().date);
+    // $("#monthsDisplay").html(snapshot.val().comment);
+    $("#monthlyDisplay").html(snapshot.val().monthlyrate);
+    // $("#totalDisplay").html(snapshot.val().comment);
+})
+
+
+
+
+
