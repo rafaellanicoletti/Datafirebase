07b39ac893644cfbad5b898a82b7360a19f072b2
rafaella nicoletti <rafaella_np@hotmail.com>
2019-06-01 10:57:36 -0400

    lyout

---
+++ app.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBFUM969amA_yoxOy4wzBuoHJF3Kw4GDNs",
+    authDomain: "loginform2-6b2bb.firebaseapp.com",
+    databaseURL: "https://loginform2-6b2bb.firebaseio.com",
+    projectId: "loginform2-6b2bb",
+    storageBucket: "loginform2-6b2bb.appspot.com",
+    messagingSenderId: "351574340721",
+    appId: "1:351574340721:web:0842a607cd1fd5f3"
+};
